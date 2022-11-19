c8095eec10235fff8951da59e3e27028e416b7ea
Rahul Rai <rahulrai@live.com>
2022-11-19 15:06:34 +1100

    updated metric

--- 2-instrumented/vote-ui/AppMetrics.cs
+++ 2-instrumented/vote-ui/AppMetrics.cs
@@ -7,7 +7,7 @@ public class AppMetrics
     public AppMetrics(string meterName, string meterVersion)
     {
         var meter = new Meter(meterName, meterVersion);
-        VoteCounter = meter.CreateCounter<long>("vote_count", "count", "Number of votes cast");
+        VoteCounter = meter.CreateCounter<long>("vote", "count", "Number of votes cast");
     }
 
     public Counter<long> VoteCounter { get; }
