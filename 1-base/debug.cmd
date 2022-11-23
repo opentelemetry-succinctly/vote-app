21c35dac12cc88cd90e58404222553be16ffd4b0
Rahul Rai <rahulrai@live.com>
2022-11-23 19:11:38 +1100

    debug

--- 1-base/debug.cmd
+++ 1-base/debug.cmd
@@ -1,3 +1,3 @@
-REM Launch the dependencies before debugging the application without Docker Compose.
+REM Ensure that the dependencies are launched before debugging the application if you are not using the Docker Compose setup.
 docker run -d --hostname my-rabbitmq-server --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
 docker run -d --name redis -p 6379:6379 redis:6.2 
---
+++ 2-instrumented/debug.cmd
@@ -0,0 +1,4 @@
+REM Ensure that the dependencies are launched before debugging the application if you are not using the Docker Compose setup.
+docker run -d --hostname my-rabbitmq-server --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
+docker run -d --name redis -p 6379:6379 redis:6.2 
+docker run -d --name jaeger -p 16686:16686 -p 6831:6831/udp jaegertracing/all-in-one:1.39
