db044e4e25e31d014af19b002df853fc1ab09b8b
Rahul Rai <rahulrai@live.com>
2023-01-06 14:12:11 +1100

    fix name

--- 2-instrumented/vote-ui/Data/VoteService.cs
+++ 2-instrumented/vote-ui/Data/VoteService.cs
@@ -53,45 +53,45 @@ public class VoteService
         return (response!.Vote1, response.Vote2);
     }
 
-    public void IncrementVote(int candidate)
+public void IncrementVote(int candidate)
+{
+    _logger.LogInformation("Received vote for candidate {candidate}", candidate);
+    var tags = new TagList
     {
-        _logger.LogInformation("Received vote for candidate {candidate}", candidate);
-        var tags = new TagList
-        {
-            { "user-agent", _contextAccessor.HttpContext?.Request.Headers.UserAgent },
-            { "host", _contextAccessor.HttpContext?.Request.Headers.Host.ToString() },
-        };
-        _appMetrics.VoteCounter.Add(1, tags);
+        { "user-agent", _contextAccessor.HttpContext?.Request.Headers.UserAgent },
+        { "host", _contextAccessor.HttpContext?.Request.Headers.Host.ToString() },
+    };
+    _appMetrics.VoteCounter.Add(1, tags);
 
-        var userAgent = _contextAccessor.HttpContext?.Request.Headers.UserAgent;
-        var host = _contextAccessor.HttpContext?.Request.Headers.Host;
+    var userAgent = _contextAccessor.HttpContext?.Request.Headers.UserAgent;
+    var host = _contextAccessor.HttpContext?.Request.Headers.Host;
 
-        using var span = _tracer.StartActiveSpan("RabbitMq publish", SpanKind.Producer);
-        span.AddEvent($"Received vote from {userAgent}");
-        span.SetAttribute("messaging.system", "rabbitmq");
-        span.SetAttribute("messaging.destination_kind", "queue");
-        span.SetAttribute("messaging.rabbitmq.queue", "sample");
+    using var span = _tracer.StartActiveSpan("RabbitMq publish", SpanKind.Producer);
+    span.AddEvent($"Received vote from {userAgent}");
+    span.SetAttribute("messaging.system", "rabbitmq");
+    span.SetAttribute("messaging.destination_kind", "queue");
+    span.SetAttribute("messaging.rabbitmq.queue", _config["Queue:Host"]);
 
-        // Prepare baggage for transfer to message consumer
-        Baggage.SetBaggage("ClientUserAgent", userAgent);
-        Baggage.SetBaggage("ClientHost", host);
+    // Prepare baggage for transfer to message consumer
+    Baggage.SetBaggage("ClientUserAgent", userAgent);
+    Baggage.SetBaggage("ClientHost", host);
 
-        // Refer to RabbitMQ guide for best practices https://www.rabbitmq.com/dotnet-api-guide.html
-        var factory = new ConnectionFactory { HostName = _config["Queue:Host"] };
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-        var props = channel.CreateBasicProperties();
-        // add trace context to message headers
-        Propagator.Inject(new(span.Context, Baggage.Current), props, (basicProps, key, value) =>
-        {
-            basicProps.Headers ??= new Dictionary<string, object>();
-            basicProps.Headers[key] = value;
-        });
+    // Refer to RabbitMQ guide for best practices https://www.rabbitmq.com/dotnet-api-guide.html
+    var factory = new ConnectionFactory { HostName = _config["Queue:Host"] };
+    using var connection = factory.CreateConnection();
+    using var channel = connection.CreateModel();
+    var props = channel.CreateBasicProperties();
+    // add trace context to message headers
+    Propagator.Inject(new(span.Context, Baggage.Current), props, (basicProps, key, value) =>
+    {
+        basicProps.Headers ??= new Dictionary<string, object>();
+        basicProps.Headers[key] = value;
+    });
 
-        channel.QueueDeclare(_config["Queue:Name"], autoDelete: false, exclusive: false);
-        channel.BasicPublish(string.Empty, "votes", body: BitConverter.GetBytes(candidate), basicProperties: props);
-        _logger.LogInformation("Published message on queue for candidate {candidate}", candidate);
-    }
+    channel.QueueDeclare(_config["Queue:Name"], autoDelete: false, exclusive: false);
+    channel.BasicPublish(string.Empty, "votes", body: BitConverter.GetBytes(candidate), basicProperties: props);
+    _logger.LogInformation("Published message on queue for candidate {candidate}", candidate);
+}
 
     public async Task ResetVotesAsync()
     {
