fc9ce2a0d46fc01255981f53ab070bfcf500b4ce
Rahul Rai <rahulrai@live.com>
2023-01-09 18:58:15 +1100

    added comments

--- 2-instrumented/vote-ui/AppMetrics.cs
+++ 2-instrumented/vote-ui/AppMetrics.cs
@@ -6,7 +6,9 @@ public class AppMetrics
 {
     public AppMetrics(string meterName, string meterVersion)
     {
+        // Creates a new meter
         var meter = new Meter(meterName, meterVersion);
+        // Creates a new instrument from the meter
         VoteCounter = meter.CreateCounter<long>("vote", "count", "Number of votes cast");
     }
 
